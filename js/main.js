a25e86fbe26bef3c0faf3b314aa4cb9986b63243
Paula Aguirres <pau.aguirres@hotmail.com>
2023-02-23 18:05:34 -0300

    arreglo h5 de la card

--- js/main.js
+++ js/main.js
@@ -8,16 +8,16 @@ for(let event of data.events){
     } else {
         console.log("evento futuro")
     }
-    // htmlEvents += `<div class="cards">
-    //     <div class="card" style="width: 14rem;">
-    //     <img src="${event.image}" class="card-img-top" alt="...">
-    //     <div class="card-body">
-    //         <h5 class="${event.name}">Card title</h5>
-    //         <p class="card-text">${event.description}</p>
-    //         <p>${event.price}</p>
-    //         <a href="./Details.html" class="btn btn-primary">Ver más...</a>
-    //     </div>
-    // </div>`;
+    htmlEvents += `<div class="cards">
+        <div class="card" style="width: 14rem;">
+        <img src="${event.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title"${event.name}</h5>
+            <p class="card-text">${event.description}</p>
+            <p>${event.price}</p>
+            <a href="./Details.html" class="btn btn-primary">Ver más...</a>
+        </div>
+    </div>`;
 }
 
 console.log(htmlEvents);

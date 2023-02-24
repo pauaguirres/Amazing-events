9e41291c49a32225e20bbb8efb68d948305b5a8d
Paula Aguirres <pau.aguirres@hotmail.com>
2023-02-24 08:29:55 -0300

    creacion past.js y upcoming.js

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ for(let event of data.events){
         <div class="card" style="width: 14rem;">
         <img src="${event.image}" class="card-img-top" alt="...">
         <div class="card-body">
-        <h5 class="card-title"${event.name}</h5>
+        <h5 class="card-title">${event.name}</h5>
             <p class="card-text">${event.description}</p>
             <p>${event.price}</p>
             <a href="./Details.html" class="btn btn-primary">Ver más...</a>
